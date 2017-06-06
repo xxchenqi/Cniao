7c82530b8d89dee5847383e1c014dbe2ccadd4b7
chen.qi <812046652@qq.com>
2017-06-06 15:27:46 +0800

    首次提交

---
+++ app/src/main/java/com/cniao/MainActivity.java
@@ -0,0 +1,90 @@
+package com.cniao;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
+        View headerView = mNavigationView.getHeaderView(0);
+        headerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "head", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menu_app_update:
+                        Toast.makeText(MainActivity.this, "应用更新", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_message:
+                        Toast.makeText(MainActivity.this, "消息", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_setting:
+                        Toast.makeText(MainActivity.this, "设置", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @OnClick({R.id.drawer_layout, R.id.navigation_view})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.drawer_layout:
+                break;
+            case R.id.navigation_view:
+                break;
+        }
+    }
+}
